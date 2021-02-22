9a5dd93514906477b879388749b3b67013c1435d
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-02-22 19:56:40 +0000

    Create authentication for valid and invalid user

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,13 +1,31 @@
 class UserAuthenticator
-  AuthenticationError = Class.new(StandardError)
+  class AuthenticationError < StandardError; end
+  attr_reader :user
 
   def initialize(code)
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: ENV['GIHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present? #only try if there is error on token [token.error] if no error return nil
+      raise AuthenticationError
+    else
+      #Terminal 
+      #user_registration
+      #client = Octokit::Client.new(client_id: 'xxx', client_secret: 'xxx')
+      #client.user('richill').to_h.slice(:logiin, :avatar_url, :url, :name)
+      user_client = Octokit::Client.new(access_token: token)
+      user_data = client.user(user_client).to_h.slice(:login, :avatar_url, :url, :name)
+      User.create(user_data.merge(provider: 'github'))
+    end
   end
 
-  def user
-  end
+  private
+
+  attr_reader :code
 end
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,12 +2,51 @@ require 'rails_helper'
 
 RSpec.describe UserAuthenticator do
   describe '#perform' do
+    let(:authenticator) { described_class.new('sample_code') }
+
+    subject { authenticator.perform }
+
     context 'when code is incorrect' do
+      let(:error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(error)
+      end
+
       it 'should raise an error' do
-        authenticator = described_class.new('sample_code')
-        expect{ authenticator.perform }.to raise_error(UserAuthenticator::AuthenticationError)
+        expect{ subject }.to raise_error(UserAuthenticator::AuthenticationError)
         expect(authenticator.user).to be_nil
+      end
+    end
+
+    context 'when code is correct' do
+      let(:user_data) do 
+        {
+          login: 'jsmith1', 
+          avatar_url: 'http://example.com/avatar', 
+          url: 'http://example.com',
+          name: 'John Smith'
+        }
+      end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return('validaccesstoken')
+        allow_any_instance_of(Octokit::Client).to receive(:user).and_return(user_data)
+      end
+
+      it 'should save the user when the user does not exist' do
+        expect{ subject }.to change{ User.count }.by(1)
+        expect(User.last.name).to eq('John Smith')
       end
     end
   end  
 end
+
+# Terminal
+# 2.7.0 :004 > token = client.exchange_code_for_token('invalidcode')
+# 2.7.0 :005 > token.class.name
+# => "Sawyer::Resource" 
+# 2.7.0 :006 > token.error
+# => "bad_verification_code" 
