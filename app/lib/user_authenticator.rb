1ece069442c5472c174aa93af847f64dc248e46f
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-02-22 18:04:08 +0000

    Add methods perform, user and AuthenticationError

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,5 +1,13 @@
 class UserAuthenticator
+  AuthenticationError = Class.new(StandardError)
+
   def initialize(code)
-    
+  end
+
+  def perform
+    raise AuthenticationError
+  end
+
+  def user
   end
 end
